04423d7ef4e82ca284f1879d0c5e40bf6ab80262
petarpejo32 <petar.pejoski32@gmail.com>
2019-10-14 21:55:36 +0200

    redux1

--- my-app/src/Redux/store.js
+++ my-app/src/Redux/store.js
@@ -1,6 +1,9 @@
-import {createStore} from 'redux'
+import {createStore, combineReducers} from 'redux'
 import userReducer from './reducers/userReducer.js'
 
+const singleReducer=combineReducers({
+    userReducer,
+})
 const store = createStore(userReducer)
 
 console.log(store.getState())
